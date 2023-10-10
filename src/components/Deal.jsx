1764f5129f87e9b5f3ee9283dcf932818259f827
Will Moretz <whmoretz@gmail.com>
2023-10-10 15:45:00 -0400

    used useFetch hook

--- src/components/Deal.jsx
+++ src/components/Deal.jsx
@@ -1,6 +1,14 @@
+import useFetch from "../useFetch";
+
 // Displays a deal from a specific store for a specific game
-function Deal() {
-  return <div>NOT IMPLEMENTED: DEAL</div>;
+function Deal({ id }) {
+  const [data, error, loading] = useFetch(
+    `https://www.cheapshark.com/api/1.0/deals?id=${id}`
+  );
+
+  if (loading) return <div>loading...</div>;
+  if (error !== null) return <div>error</div>;
+  return <div>{data.gameInfo.name}</div>;
 }
 
 export default Deal;
