f2292b952233731a44406cc477fea41970248cb2
Will Moretz <whmoretz@gmail.com>
2023-10-12 07:58:13 -0400

    added store component, fetch call for store

--- src/components/StoreCard.jsx
+++ src/components/StoreCard.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
+import Store from "./Store";
+
 // Displays a game store and some of its top rated deals
-function StoreCard() {
-  return <div>NOT IMPLEMENTED: STORE CARD</div>;
+function StoreCard({ id }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://www.cheapshark.com/api/1.0/deals?storeID=${id}`
+      );
+      const result = await response.json();
+      setData(result);
+    })();
+  }, [id]);
+
+  return <Store data={data} />;
 }
 
 export default StoreCard;
