71e03598b7681d2e8908e122788e91a0c602106c
Will Moretz <whmoretz@gmail.com>
2023-10-12 07:57:50 -0400

    added store id array

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,10 @@
+import StoreCard from "./StoreCard";
+
+const STORE_IDS = [1, 4, 25, 7, 11, 3, 5, 15];
+
 // Displays the home page, the landing page
 function HomePage() {
-  return <div>NOT IMPLEMENTED: HOME PAGE</div>;
+  return <StoreCard id={STORE_IDS[0]} />;
 }
 
 export default HomePage;
