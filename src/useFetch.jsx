6b880c5c0cb88a0fad86c5a05668f643b4e58065
Will Moretz <whmoretz@gmail.com>
2023-10-12 12:48:36 -0400

    added clean function and request cancelling

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -6,17 +6,27 @@ function useFetch(url) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (!url) return;
+
+    let cancelRequest = false;
     (async () => {
       try {
         const response = await fetch(url);
         const result = await response.json();
+        if (cancelRequest) return;
         setData(result);
       } catch (error) {
+        if (cancelRequest) return;
         setError(error);
       } finally {
+        if (cancelRequest) return;
         setLoading(false);
       }
     })();
+
+    return function cleanup() {
+      cancelRequest = true;
+    };
   }, [url]);
 
   return [data, error, loading];
