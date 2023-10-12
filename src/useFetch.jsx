f47c13310aa48db7382c4dc9d9a534cf5a95fd61
Will Moretz <whmoretz@gmail.com>
2023-10-12 10:19:25 -0400

    created useFetch hook

---
+++ src/useFetch.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
+
+  return [data, error, loading];
+}
+
+export default useFetch;
