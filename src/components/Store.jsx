6f195d155edf988f1a5211a4a8061562fb04d26e
Will Moretz <whmoretz@gmail.com>
2023-10-13 07:55:46 -0400

    added displays for more deal data

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom";
+
 // Displays a game store
 function Store({ data }) {
-  return <div>{data[0].title}</div>;
+  return (
+    <div>
+      <img src={data.thumb} />
+      <span>{data.title} </span>
+      <span>${data.salePrice} </span>
+      <span>{Math.round(data.savings)}%</span>
+      <Link to={`deal/${data.dealID}`}>link</Link>
+    </div>
+  );
 }
 
 export default Store;
