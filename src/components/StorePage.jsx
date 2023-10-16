78784dab0e740a1dab7f519a3c7b739bf823dda3
Will Moretz <whmoretz@gmail.com>
2023-10-16 13:21:37 -0400

    added redirect on bad url

--- src/components/StorePage.jsx
+++ src/components/StorePage.jsx
@@ -1,15 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "../useFetch";
 import StoreItem from "./StoreItem";
 
 function StorePage() {
   const params = useParams();
+  const navigate = useNavigate();
+
   const [data, error, loading] = useFetch(
     `https://www.cheapshark.com/api/1.0/deals?storeID=${params.id}`
   );
 
   if (loading) return <div>loading...</div>;
   if (error) return <div>{error.message || "Load Failed"}</div>;
+  if (data.length === 0) return navigate("/"); //TODO: Navigate to error page
 
   return (
     <>
