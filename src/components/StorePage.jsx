f5935a964bce09a8d58b0b6bb3331b7a7896c6ab
Will Moretz <whmoretz@gmail.com>
2023-10-16 12:06:28 -0400

    added fetching and displaying of store data

--- src/components/StorePage.jsx
+++ src/components/StorePage.jsx
@@ -1,5 +1,28 @@
+import { useParams, Link } from "react-router-dom";
+import useFetch from "../useFetch";
+
 function StorePage() {
-  return <div>NOT IMPLEMENTED: STORE PAGE</div>;
+  const params = useParams();
+  const [data, error, loading] = useFetch(
+    `https://www.cheapshark.com/api/1.0/deals?storeID=${params.id}`
+  );
+
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>{error.message || "Load Failed"}</div>;
+
+  return (
+    <>
+      {data.map((game) => (
+        <div key={game.dealID}>
+          <img src={game.thumb} alt={`${game.title} thumbnail`} />
+          <span>{game.title} </span>
+          <span>${game.salePrice} </span>
+          <span>{Math.round(game.savings)}%</span>
+          <Link to={`/deal/${game.dealID}`}>link</Link>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default StorePage;
