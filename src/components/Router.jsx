b0c32c645665925c1f9d93fb744e4c7ef0109083
Will Moretz <whmoretz@gmail.com>
2023-10-16 12:02:28 -0400

    added storepage route

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -3,6 +3,7 @@ import HomePage from "./HomePage";
 import ErrorPage from "./ErrorPage";
 import BrowsePage from "./BrowsePage";
 import AboutPage from "./AboutPage";
+import StorePage from "./StorePage";
 
 // Routes the user to page components based on the url
 function Router() {
@@ -23,6 +24,10 @@ function Router() {
     {
       path: "/deal/:id",
       element: <ErrorPage />,
+    },
+    {
+      path: "stores/:id",
+      element: <StorePage />,
     },
   ]);
 
