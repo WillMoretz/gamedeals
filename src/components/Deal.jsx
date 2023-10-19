2fe72dfa48f942e186069aac841560511f97215c
Will Moretz <whmoretz@gmail.com>
2023-10-19 13:49:56 -0400

    created deal page component

--- src/components/Deal.jsx
+++
@@ -1,6 +0,0 @@
-// Displays a deal from a specific store for a specific game
-function Deal() {
-  return <div>NOT IMPLEMENTED: DEAL</div>;
-}
-
-export default Deal;
---
+++ src/components/DealPage.jsx
@@ -0,0 +1,6 @@
+// Displays a deal from a specific store for a specific game
+function DealPage() {
+  return <div>NOT IMPLEMENTED: DEAL PAGE</div>;
+}
+
+export default DealPage;
