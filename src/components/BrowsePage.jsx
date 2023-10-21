9c449100ed6919d9d4008d1cb05bc79246462a21
Will Moretz <whmoretz@gmail.com>
2023-10-21 10:30:24 -0400

    added useSearchParams hook

--- src/components/BrowsePage.jsx
+++ src/components/BrowsePage.jsx
@@ -1,5 +1,11 @@
+import { useSearchParams } from "react-router-dom";
+
 // Displays all of the game stores
 function StorePage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams);
+  console.log(searchParams.toString());
+
   return <div>NOT IMPLEMENTED: STORE PAGE</div>;
 }
 
