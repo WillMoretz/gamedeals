48a3c1cd91ac0997ebcc1073c6049297936d77eb
Will Moretz <whmoretz@gmail.com>
2023-10-21 17:29:09 -0400

    cleanup

--- src/components/BrowsePage.jsx
+++ src/components/BrowsePage.jsx
@@ -3,6 +3,8 @@ import useFetch from "../useFetch";
 import StoreItem from "./StoreItem";
 import filterRepeatDeals from "../filterRepeatDeals";
 
+const OPTION_PARAMS = ["filter"];
+
 // Displays all of the game stores
 function BrowsePage() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -11,15 +13,15 @@ function BrowsePage() {
   let query = "";
   for (const param of searchParams) {
     // Skip params unrelated to API
-    if (param[0] !== "filter") {
+    if (!OPTION_PARAMS.includes(param[0])) {
       // Add param to the query, if its not the first param include the "&" character
-      if (query === "") query += `${param[0]}=${param[1]}`;
+      if (query === "") query += `?${param[0]}=${param[1]}`;
       else query += `&${param[0]}=${param[1]}`;
     }
   }
 
   const [data, error, loading] = useFetch(
-    `https://www.cheapshark.com/api/1.0/deals?${query}`
+    `https://www.cheapshark.com/api/1.0/deals${query}`
   );
 
   if (loading) return <div>loading...</div>;
