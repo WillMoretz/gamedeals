95bf4c5a95775716a7e7446ecdbb8d136ae8cd5d
Will Moretz <whmoretz@gmail.com>
2023-10-21 12:34:35 -0400

    added fetching and displaying of data

--- src/components/BrowsePage.jsx
+++ src/components/BrowsePage.jsx
@@ -1,12 +1,28 @@
 import { useSearchParams } from "react-router-dom";
+import useFetch from "../useFetch";
+import StoreItem from "./StoreItem";
+import filterRepeatDeals from "../filterRepeatDeals";
 
 // Displays all of the game stores
-function StorePage() {
+function BrowsePage() {
   const [searchParams, setSearchParams] = useSearchParams();
-  console.log(searchParams);
-  console.log(searchParams.toString());
 
-  return <div>NOT IMPLEMENTED: STORE PAGE</div>;
+  const [data, error, loading] = useFetch(
+    `https://www.cheapshark.com/api/1.0/deals?${searchParams.toString()}`
+  );
+
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>{error.message || "Load Failed"}</div>;
+
+  const filteredData = filterRepeatDeals(data);
+
+  return (
+    <>
+      {filteredData.map((deal) => (
+        <StoreItem deal={deal} key={deal.dealID} />
+      ))}
+    </>
+  );
 }
 
-export default StorePage;
+export default BrowsePage;
