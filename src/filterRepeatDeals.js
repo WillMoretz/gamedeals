5d8df2418f998d833658b59f9a91aa7fb7b830a3
Will Moretz <whmoretz@gmail.com>
2023-10-22 23:15:06 -0400

    formatting

--- src/filterRepeatDeals.js
+++ src/filterRepeatDeals.js
@@ -6,20 +6,22 @@ export default function filterRepeatDeals(dealArray, responseNumTarget = 60) {
   let responseNum = 0;
 
   for (let i = 0; i < dealArray.length; i++) {
-    // Detects repeats and uses cheaper deal in the response
+    // Detects repeats
     if (dealArray[i].title in response) {
+      // Uses cheaper deal in the response
       if (
         parseFloat(response[dealArray[i].title].salePrice) >
         parseFloat(dealArray[i].salePrice)
-      )
+      ) {
         response[dealArray[i].title] = dealArray[i];
+      }
       continue;
     }
-
     response[dealArray[i].title] = dealArray[i];
 
     responseNum++;
     if (responseNum === responseNumTarget) break;
   }
+
   return Object.values(response);
 }
