707111e70103c719d237ce927b95e51baaf3c27c
Will Moretz <whmoretz@gmail.com>
2023-10-25 07:15:49 -0400

    added line to catch urls with no params

--- src/cache.js
+++ src/cache.js
@@ -6,6 +6,7 @@ function addCacheItem(key, value) {
 
 function sortParamString(paramStr) {
   const split1 = paramStr.split("?");
+  if (!split1[1]) return split1[0];
   const split2 = split1[1].split("&");
   const sortedArr = split2.sort();
   return `${split1[0]}?${sortedArr.join("&")}`;
